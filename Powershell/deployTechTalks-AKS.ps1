acef27f4fa82bc8cda5fc8f1b89c363f78df9df7
Nishant Meher <nishant.meher@roundpointmortgage.com>
2020-09-15 02:04:52 +0530

    deployment changes

--- Powershell/deployTechTalks-AKS.ps1
+++ Powershell/deployTechTalks-AKS.ps1
@@ -28,4 +28,4 @@ Write-Host "Tech talks Producer deployed successfully" -ForegroundColor Cyan
 
 Write-Host "All the services related to Tech Talks application have been successfully deployed" -ForegroundColor Cyan
 
-Set-Location ~/KEDA/Powershell
+Set-Location C:\CODE\KEDA\Powershell
